a3095c0b34f5de130955426e6051c4964fe3236b
loverplaya <maxim.bryzgin@yandex.ru>
2026-02-17 12:29:43 +0300

    add MainWindow class

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world")
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        self.setGeometry(600, 200, 800, 600)
+        self.setWindowTitle("Прогноз погоды")
+        self.setUpMainWindow()
+        self.show()
+
+    def setUpMainWindow(self):
+
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+sys.exit(app.exec())
